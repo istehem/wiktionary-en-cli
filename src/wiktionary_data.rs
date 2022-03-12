d4a60298123623d1de2a80c44478623f2eb4d825
Oskar Ingemarsson <oskar.ingemarsson[at]gmail.com>
2022-03-12 21:17:51 +0100

    moved colored string join code

--- src/wiktionary_data.rs
+++ src/wiktionary_data.rs
@@ -6,32 +6,10 @@ use colored::ColoredString;
 use textwrap::{fill, indent};
 
 pub mod language;
+pub mod colored_string_utils;
+use crate::colored_string_utils::Join;
 
 static LINE_WRAP_AT: usize = 80;
-
-trait Join {
-    fn join(&self, list : Vec<Self>) -> Self where Self: Sized;
-    fn joinwrap(&self, list : Vec<Self>, width : usize) -> Self where Self: Sized;
-}
-
-impl Join for ColoredString {
-    fn join(&self, list : Vec<ColoredString>) -> ColoredString {
-        let mut res : ColoredString = "".normal();
-        let len : usize = list.len();
-        for (i, string) in list.iter().enumerate() {
-            res = format!("{}{}", res, string).normal();
-            if i < len - 1 {
-                res = format!("{}{}", res, self).normal();
-            }
-        }
-        return res.clone();
-    }
-
-    fn joinwrap(&self, list : Vec<ColoredString>, width : usize) -> ColoredString {
-        let text = self.join(list);
-        return fill(&text, width).normal();
-    }
-}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct DictionaryEntry {
