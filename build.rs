7d749fd132349a392bcc4718e86c2dd4f1f232ed
istehem <oskar@oskar.me>
2024-02-18 10:59:38 +0100

    better readability

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
+fn set_rusc_env_variable(key: &str, value: String) {
+    println!("cargo:rustc-env={}={}", key, value);
+}
+
 fn main() {
-    println!(
-        "cargo:rustc-env=CACHING_DIR={}/{}",
-        env!("CARGO_MANIFEST_DIR"),
-        "cache"
-    )
+    set_rusc_env_variable(
+        "CACHING_DIR",
+        format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "cache"),
+    );
 }
