c783803f4ee299178770c9ea6f7ecd17f37d1640
Ingemarsson Oskar <oskar@oskar.me>
2024-11-03 15:33:37 +0100

    parse end loop

--- sonic-import/src/main.rs
+++ sonic-import/src/main.rs
@@ -1,10 +1,13 @@
 use anyhow::{bail, Context, Result};
-use std::path::Path;
-use utilities::anyhow_serde;
+use std::io::{prelude::*, BufReader};
+use std::path::{Path, PathBuf};
 use utilities::file_utils;
+use utilities::file_utils::*;
 use utilities::language::*;
 
 use wiktionary_entities::wiktionary_entity::*;
+
+use std::fs::File;
 
 fn parse_line(line: Result<String, std::io::Error>, i: usize) -> Result<DictionaryEntry> {
     return line
@@ -14,11 +17,27 @@ fn parse_line(line: Result<String, std::io::Error>, i: usize) -> Result<Dictiona
         });
 }
 
+fn parse_and_persist(file_reader: BufReader<File>) -> Result<()> {
+    for (i, line) in file_reader.lines().enumerate() {
+        let _dictionary_entry = parse_line(line, i);
+    }
+    return Ok(());
+}
+
 pub fn do_import(path: &Path) -> Result<()> {
     match file_utils::get_file_reader(path) {
-        Ok(_) => return Ok(()),
+        Ok(path) => return parse_and_persist(path),
         _ => bail!("No such DB file: '{}'", path.display()),
     }
+}
+
+fn get_db_path(path: Option<String>, language: &Option<Language>) -> PathBuf {
+    if let Some(path) = path {
+        return PathBuf::from(path);
+    }
+    return PathBuf::from(utilities::DICTIONARY_DB_PATH!(language
+        .unwrap_or_default()
+        .value()));
 }
 
 fn main() -> Result<()> {
@@ -29,5 +48,6 @@ fn main() -> Result<()> {
         utilities::DICTIONARY_CACHING_PATH!(Language::EN.value())
     );
     println!("{}", utilities::DICTIONARY_DB_PATH!(Language::EN.value()));
-    return Ok(());
+    let db_path: PathBuf = get_db_path(None, &Some(Language::EN));
+    return do_import(db_path.as_path());
 }
