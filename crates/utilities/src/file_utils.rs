22b643a65beb3b734e21e91817dd5023aba8a2d7
Ingemarsson Oskar <oskar@oskar.me>
2024-11-18 21:36:22 +0100

    sugar

--- crates/utilities/src/file_utils.rs
+++ crates/utilities/src/file_utils.rs
@@ -1,10 +1,10 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
 pub fn get_file_reader(path: &Path) -> Result<BufReader<File>> {
-    return File::open(path).map(|f| BufReader::new(f)).map_err(|err| {
-        anyhow::Error::new(err).context(format!("Couldn't open file: '{}'", path.display()))
-    });
+    return File::open(path)
+        .map(|f| BufReader::new(f))
+        .map_err(|err| anyhow!(err).context(format!("Couldn't open file: '{}'", path.display())));
 }
