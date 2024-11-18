fb77e2504866d1fbe3e0f15c6dbd51223ccf0279
Ingemarsson Oskar <oskar@oskar.me>
2024-11-18 21:04:51 +0100

    some improvements

--- crates/utilities/src/file_utils.rs
+++ crates/utilities/src/file_utils.rs
@@ -1,14 +1,10 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
 pub fn get_file_reader(path: &Path) -> Result<BufReader<File>> {
-    let file_buffer_result = File::open(path)
-        .map(|f| BufReader::new(f))
-        .map_err(|err| anyhow::Error::new(err));
-    match file_buffer_result {
-        ok @ Ok(_) => return ok,
-        Err(err) => bail!(err.context(format!("Coldn't open DB file: '{}'", path.display()))),
-    }
+    return File::open(path).map(|f| BufReader::new(f)).map_err(|err| {
+        anyhow::Error::new(err).context(format!("Coldn't open DB file: '{}'", path.display()))
+    });
 }
