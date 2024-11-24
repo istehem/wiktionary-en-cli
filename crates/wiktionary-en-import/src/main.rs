ef00b1f2aab2376924115a02bf1f3c14035e42f1
Ingemarsson Oskar <oskar@oskar.me>
2024-11-24 14:26:00 +0100

    help message corrected

--- crates/wiktionary-en-import/src/main.rs
+++ crates/wiktionary-en-import/src/main.rs
@@ -7,7 +7,7 @@ use wiktionary_en_db::wiktionary_en_db::*;
 
 use clap::Parser;
 
-/// Import Dictionary Data into Sonic
+/// Import Dictionary Data into PoloDB
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
