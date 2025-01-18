74743d6ebc9ff1467ac70d41f3fe3295f3230eef
Ingemarsson Oskar <oskar@oskar.me>
2025-01-18 22:08:43 +0100

    can download huge files

--- crates/wiktionary-en-download/src/lib.rs
+++ crates/wiktionary-en-download/src/lib.rs
@@ -1,16 +1,35 @@
-use anyhow::{Result};
+use anyhow::Result;
 use std::fs::File;
 use std::io::Write;
+use std::time::Duration;
 
-pub fn download() -> Result<()> {
-    //https://kaikki.org/dictionary/English/kaikki.org-dictionary-English.jsonl
+use futures_util::StreamExt;
+use std::io::BufWriter;
 
-    let response = reqwest::blocking::get("https://thewowstyle.com/wp-content/uploads/2015/01/free-beautiful-place-wallpaper-hd-173.jpg")?
-        .bytes()?;
+#[tokio::main]
+async fn stream_download() -> Result<()> {
+    let client = reqwest::Client::new();
 
-    let mut f = File::create("remove-me.jpg")?;
-    f.write_all(&response)?;
+    let url = "https://kaikki.org/dictionary/English/kaikki.org-dictionary-English.jsonl";
+    //let url = "https://www.pixelstalk.net/wp-content/uploads/2016/08/Cute-Girl-HD-Images.jpg";
+    let mut bytes = client
+        .get(url)
+        .timeout(Duration::from_secs(1800))
+        .send()
+        .await
+        .unwrap()
+        .bytes_stream();
+
+    let file = File::create("test.jsonl")?;
+    let mut writer: BufWriter<File> = BufWriter::new(file);
+
+    while let Some(item) = bytes.next().await {
+        writer.write(&item?)?;
+    }
+
     return Ok(());
 }
 
-
+pub fn download() -> Result<()> {
+    return stream_download();
+}
