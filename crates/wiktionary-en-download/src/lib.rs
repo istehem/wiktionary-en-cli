ea523f8a55e087f76b2582e20bb5095f1fd6561c
Ingemarsson Oskar <oskar@oskar.me>
2025-01-18 23:39:57 +0100

    flush the file BufWriter

--- crates/wiktionary-en-download/src/lib.rs
+++ crates/wiktionary-en-download/src/lib.rs
@@ -10,18 +10,18 @@ async fn stream_download() -> Result<()> {
     let client = reqwest::Client::new();
 
     //let url = "https://kaikki.org/dictionary/English/kaikki.org-dictionary-English.jsonl";
-    let url = "https://kaikki.org/dictionary/German/kaikki.org-dictionary-German.jsonl";
+    //let url = "https://kaikki.org/dictionary/German/kaikki.org-dictionary-German.jsonl";
+    let url = "https://kaikki.org/dictionary/Swedish/kaikki.org-dictionary-Swedish.jsonl";
     //let url = "https://www.pixelstalk.net/wp-content/uploads/2016/08/Cute-Girl-HD-Images.jpg";
     let mut bytes = client.get(url).send().await?.bytes_stream();
 
-    let file = File::create("wiktionary-de.jsonl")?;
+    let file = File::create("wiktionary-sv.jsonl")?;
     let mut writer: BufWriter<File> = BufWriter::new(file);
 
     while let Some(item) = bytes.next().await {
         writer.write(&item?)?;
     }
-
-    return Ok(());
+    return writer.flush().map_err(|err| anyhow::Error::new(err));
 }
 
 pub fn download() -> Result<()> {
