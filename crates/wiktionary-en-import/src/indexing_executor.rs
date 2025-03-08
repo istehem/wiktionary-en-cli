d4cb4d70a28771d13f98faa6792cb271637788d6
Ingemarsson Oskar <oskar@oskar.me>
2025-03-09 00:53:03 +0100

    only show error message if available

--- crates/wiktionary-en-import/src/indexing_executor.rs
+++ crates/wiktionary-en-import/src/indexing_executor.rs
@@ -2,21 +2,20 @@ use anyhow::{bail, Result};
 use indicatif::{ProgressBar, ProgressStyle};
 use streaming_iterator::*;
 
-const PROGRESS_BAR_TEMPLATE: &str = "{spinner} {elapsed}\nLatest error: {msg}";
-const NO_LATEST_ERROR: &str = "<None>";
+const PROGRESS_BAR_TEMPLATE: &str = "{spinner} {elapsed}{msg}";
 
 pub fn execute_with_progress_bar_and_message<X: std::fmt::Display>(
     mut iterator: impl StreamingIterator<Item = Result<Option<X>>>,
 ) -> Result<Vec<String>> {
     let mut errors: Vec<String> = Vec::new();
     let progress_bar = ProgressBar::no_length()
-        .with_message(NO_LATEST_ERROR)
         .with_style(ProgressStyle::default_spinner().template(PROGRESS_BAR_TEMPLATE)?);
     while let Some(item) = iterator.next() {
         if let Ok(Some(item)) = item {
             let error_message = format!("{}", item);
+            let progress_bar_message = format!("\n Latest Error: {}", error_message);
             errors.push(error_message.clone());
-            progress_bar.set_message(error_message);
+            progress_bar.set_message(progress_bar_message);
         } else if let Err(err) = item {
             // “{:?}” includes a backtrace if one was captured
             bail!(format!("{:?}", err));
