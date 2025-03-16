f5ef61f29cee2df9922cb40ecb28ce6338267cef
Ingemarsson Oskar <oskar@oskar.me>
2025-03-16 12:57:56 +0100

    tracing tests

--- tests/test_uniqueness.rs
+++ tests/test_uniqueness.rs
@@ -1,17 +1,44 @@
 #[cfg(test)]
 mod tests {
-    use anyhow::Result;
+    use anyhow::{bail, Context, Result};
+    use std::collections::HashSet;
     use std::fs::File;
+    use std::io::BufRead;
     use std::io::BufReader;
     use std::path::PathBuf;
+    use tracing::info;
+    use tracing_test::traced_test;
     use utilities::file_utils;
     use utilities::language::*;
+    use wiktionary_en_entities::wiktionary_entity::*;
 
+    fn parse_line(line: &String, i: usize) -> Result<DictionaryEntry> {
+        parse_entry(line).with_context(|| format!("Couldn't parse line {} in DB file.", i))
+    }
+    #[traced_test]
     #[test]
     fn at_least_as_many_unique_entries_as_all_entries() -> Result<()> {
         let language = Language::EN;
         let db_path = PathBuf::from(utilities::DICTIONARY_DB_PATH!(language.value()));
         let file_reader: BufReader<File> = file_utils::get_file_reader(&db_path)?;
+        let mut unique_entries = HashSet::new();
+        let mut count = 0;
+        for (i, line) in file_reader.lines().enumerate() {
+            match line {
+                Ok(ok_line) => {
+                    let dictionary_entry = parse_line(&ok_line, i)?;
+                    unique_entries.insert(dictionary_entry.word);
+                }
+                _ => bail!("couldn't read line {}", i),
+            }
+            count = count + 1;
+        }
+        info!("there are {} total entries in the db file", count);
+        info!(
+            "there are {} unique entries in the db file",
+            unique_entries.len()
+        );
+        assert!(count >= unique_entries.len());
         Ok(())
     }
 }
