17532fb08879069ff9e2ed06b0f6f8b565a7a1be
Ingemarsson Oskar <oskar@oskar.me>
2025-03-29 22:29:47 +0100

    assert the actual lua output

--- crates/wiktionary-en-lua/src/lib.rs
+++ crates/wiktionary-en-lua/src/lib.rs
@@ -3,14 +3,14 @@ use mlua::Lua;
 
 fn on_plus_one(lua: &Lua) -> mlua::Result<u8> {
     let result: u8 = lua.load("return 1 + 1").eval()?;
-    println!("lua return the result: {}", result);
+    println!("lua returns the result: {}", result);
     return Ok(result);
 }
 
 pub fn do_one_plus_one() -> Result<u8> {
     let lua = Lua::new();
-    if let Ok(result) = on_plus_one(&lua) {
-        return Ok(result);
+    match on_plus_one(&lua) {
+        Ok(result) => return Ok(result),
+        Err(err) => bail!(err.to_string()),
     }
-    bail!("could not execute the lua fuction");
 }
