f0e2e5af20ba1a7d0a9f2f59f7e8b7a73b26b5e5
Ingemarsson Oskar <oskar@oskar.me>
2025-05-24 23:22:44 +0200

    applied clippy hints

--- crates/utilities/src/file_utils.rs
+++ crates/utilities/src/file_utils.rs
@@ -4,7 +4,7 @@ use std::io::BufReader;
 use std::path::Path;
 
 pub fn get_file_reader(path: &Path) -> Result<BufReader<File>> {
-    return File::open(path)
-        .map(|f| BufReader::new(f))
-        .map_err(|err| anyhow!(err).context(format!("Couldn't open file: '{}'", path.display())));
+    File::open(path)
+        .map(BufReader::new)
+        .map_err(|err| anyhow!(err).context(format!("Couldn't open file: '{}'", path.display())))
 }
