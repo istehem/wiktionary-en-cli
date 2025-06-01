d4e7bde12f0c160446389cf9c468037765eeeaca
Ingemarsson Oskar <oskar@oskar.me>
2025-06-01 21:30:57 +0200

    find fields in raw dictionary files

--- tests/test_data_fields.rs
+++ tests/test_data_fields.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
     use anyhow::{bail, Context, Result};
+    use serde_json::Value;
+    use std::collections::HashSet;
     use std::fs::File;
     use std::io::BufRead;
     use std::io::BufReader;
@@ -18,11 +20,61 @@ mod tests {
         let language = Language::SV;
         let db_path = PathBuf::from(utilities::DICTIONARY_DB_PATH!(language.value()));
         let file_reader: BufReader<File> = file_utils::get_file_reader(&db_path)?;
-        for (i, line) in file_reader.lines().enumerate() {
+        for (i, line) in file_reader.lines().enumerate().take(10) {
             match line {
                 Ok(ok_line) => {
                     let dictionary_entry = parse_line(&ok_line, i)?;
                     assert_ne!(dictionary_entry.word, "");
+                }
+                _ => bail!("couldn't read line {}", i),
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn lookup_fields() -> Result<()> {
+        let language = Language::EN;
+        let db_path = PathBuf::from(utilities::DICTIONARY_DB_PATH!(language.value()));
+        let file_reader: BufReader<File> = file_utils::get_file_reader(&db_path)?;
+        let mut unique_keys = HashSet::new();
+        for (i, line) in file_reader.lines().enumerate().take(10) {
+            match line {
+                Ok(ok_line) => {
+                    let value: Value = serde_json::from_str(&ok_line)?;
+                    if let Some(obj) = value.as_object() {
+                        for key in obj.keys() {
+                            unique_keys.insert(key.clone());
+                        }
+                    }
+                }
+                _ => bail!("couldn't read line {}", i),
+            }
+        }
+        for key in unique_keys {
+            println!("found key: {}", key);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn lookup_wikipedia_fields() -> Result<()> {
+        let language = Language::SV;
+        let db_path = PathBuf::from(utilities::DICTIONARY_DB_PATH!(language.value()));
+        let file_reader: BufReader<File> = file_utils::get_file_reader(&db_path)?;
+        for (i, line) in file_reader.lines().enumerate() {
+            match line {
+                Ok(ok_line) => {
+                    let value: Value = serde_json::from_str(&ok_line)?;
+                    if let Some(obj) = value.as_object() {
+                        for key in obj.keys() {
+                            if key == "synonyms" {
+                                println!("for word: {}", obj["word"]);
+                                println!("field value is: {}", obj[key]);
+                                return Ok(());
+                            }
+                        }
+                    }
                 }
                 _ => bail!("couldn't read line {}", i),
             }
