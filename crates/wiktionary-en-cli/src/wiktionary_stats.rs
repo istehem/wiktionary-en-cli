c2d6dfea09d891149f92fc804ca42e1e531b310a
Ingemarsson Oskar <oskar@oskar.me>
2025-07-05 12:53:27 +0200

    don't ignore errors

--- crates/wiktionary-en-cli/src/wiktionary_stats.rs
+++ crates/wiktionary-en-cli/src/wiktionary_stats.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use colored::ColoredString;
 use colored::Colorize;
 use std::fmt;
@@ -29,8 +29,8 @@ const MEGABYTE: f64 = 1024.0 * 1024.0;
 pub struct Stats {
     path: String,
     database_dir: String,
-    database_entries: Option<usize>,
-    number_of_entries: Option<usize>,
+    database_entries: usize,
+    number_of_entries: usize,
     file_size: Option<f64>,
 }
 
@@ -46,12 +46,14 @@ impl Stats {
         res.push(format_key_value!("dictionary file", self.path));
         res.push(format_key_value!("database dir", self.database_dir));
 
-        if let Some(n) = self.database_entries {
-            res.push(format_key_value!("dabase entries", format_integer(n)));
-        }
-        if let Some(n) = self.number_of_entries {
-            res.push(format_key_value!("dictionary entries", format_integer(n)));
-        }
+        res.push(format_key_value!(
+            "dabase entries",
+            format_integer(self.database_entries)
+        ));
+        res.push(format_key_value!(
+            "dictionary entries",
+            format_integer(self.number_of_entries)
+        ));
         if let Some(s) = self.file_size {
             res.push(format_key_value!(
                 "dictionary size",
@@ -67,17 +69,10 @@ pub fn calculate_stats(dictionary_path: &Path, language: &Language) -> Result<St
     Ok(Stats {
         path: dictionary_path.display().to_string(),
         database_dir: String::from(DICTIONARY_POLO_DB_DIR!()),
-        database_entries: number_of_database_entries(&client),
+        database_entries: client.number_of_entries()? as usize,
         file_size: file_size_in_megabytes(dictionary_path),
-        number_of_entries: number_of_entries(dictionary_path),
+        number_of_entries: number_of_entries(dictionary_path)?,
     })
-}
-
-fn number_of_database_entries(client: &wiktionary_en_db::WiktionaryDbClient) -> Option<usize> {
-    if let Ok(number) = client.number_of_entries() {
-        return Some(number as usize);
-    }
-    None
 }
 
 fn file_size_in_megabytes(input_path: &Path) -> Option<f64> {
@@ -90,12 +85,9 @@ fn file_size_in_megabytes(input_path: &Path) -> Option<f64> {
     None
 }
 
-fn number_of_entries(input_path: &Path) -> Option<usize> {
+fn number_of_entries(input_path: &Path) -> Result<usize> {
     if input_path.is_dir() {
-        return None;
+        bail!("path {} is a directory", input_path.display());
     }
-    if let Ok(br) = get_file_reader(input_path) {
-        return Some(br.lines().count());
-    }
-    None
+    Ok(get_file_reader(input_path)?.lines().count())
 }
