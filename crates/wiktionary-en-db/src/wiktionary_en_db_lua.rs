3a65f4a383ae80988324e5bddd01ab08439ec40a
Ingemarsson Oskar <oskar@oskar.me>
2025-07-08 23:17:51 +0200

    rember that we have to use Mutex

--- crates/wiktionary-en-db/src/wiktionary_en_db_lua.rs
+++ crates/wiktionary-en-db/src/wiktionary_en_db_lua.rs
@@ -2,6 +2,14 @@ use mlua;
 
 use crate::wiktionary_en_db::WiktionaryDbClient;
 
+/*
+// Wrap in Arc and Mutex to allow shared mutable access
+let db = Arc::new(Mutex::new(DbConnection::new()));
+
+// Set the db object as a global Lua value
+lua.globals().set("db", db)?;
+*/
+
 impl mlua::UserData for WiktionaryDbClient {
     fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
         methods.add_method("write_to_history", |_, this, word: String| {
