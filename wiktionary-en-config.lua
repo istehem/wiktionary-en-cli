acdbd59314663742d9dee47f1e8c20018d6d9798
Ingemarsson Oskar <oskar@oskar.me>
2025-07-16 21:28:40 +0200

    renamed main lua files

--- wiktionary-en-config.lua
+++ wiktionary-en-config.lua
@@ -1,10 +1,3 @@
-local formatter = require("formatter")
-local interceptor = require("interceptor")
-
 config = {}
--- format Wiktionary results
-config.format_entry = formatter.format_entry
--- format Wiktionary did-you-mean banner
-config.format_did_you_mean_banner = formatter.format_did_you_mean_banner
--- intercept Wiktionary results
-config.intercept = interceptor.intercept
+-- set a default language
+config.language = "en"
--- wiktionary-en-config2.lua
+++
@@ -1,3 +0,0 @@
-config = {}
--- set a default language
-config.language = "en"
