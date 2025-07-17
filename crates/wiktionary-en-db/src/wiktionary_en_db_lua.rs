f1876fa7dcd8690356271877c3159b14cf96befe
Ingemarsson Oskar <oskar@oskar.me>
2025-07-17 22:39:58 +0200

    do not panic

--- crates/wiktionary-en-db/src/wiktionary_en_db_lua.rs
+++ crates/wiktionary-en-db/src/wiktionary_en_db_lua.rs
@@ -1,12 +1,20 @@
-use mlua;
+use mlua::Result;
+use std::sync::MutexGuard;
 
-use crate::wiktionary_en_db::WiktionaryDbClientMutex;
+use crate::wiktionary_en_db::{WiktionaryDbClient, WiktionaryDbClientMutex};
+
+fn lock<'a>(db_client: &'a WiktionaryDbClientMutex) -> Result<MutexGuard<'a, WiktionaryDbClient>> {
+    match db_client.client.lock() {
+        Ok(db_client) => Ok(db_client),
+        Err(err) => Err(mlua::Error::RuntimeError(String::from(err.to_string()))),
+    }
+}
 
 impl mlua::UserData for WiktionaryDbClientMutex {
     fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
         methods.add_method("write_to_history", |_, this, word: String| {
-            let client = this.client.lock().unwrap();
-            match client.upsert_into_history(&word) {
+            let db_client = lock(this)?;
+            match db_client.upsert_into_history(&word) {
                 Ok(_) => Ok(()),
                 Err(err) => Err(mlua::Error::RuntimeError(err.to_string())),
             }
