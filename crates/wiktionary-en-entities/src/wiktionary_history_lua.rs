a292d3dd0ebddb5c0dd0cf13b05ef9b59083c375
Ingemarsson Oskar <oskar@oskar.me>
2025-07-18 21:50:10 +0200

    lua table attribute name changed

--- crates/wiktionary-en-entities/src/wiktionary_history_lua.rs
+++ crates/wiktionary-en-entities/src/wiktionary_history_lua.rs
@@ -8,7 +8,7 @@ impl IntoLua for HistoryEntry {
     fn into_lua(self, lua: &Lua) -> mlua::Result<Value> {
         let history_entry = lua.create_table()?;
         history_entry.set("word", self.word)?;
-        history_entry.set("timestamp", self.last_hit.timestamp())?;
+        history_entry.set("last_hit", self.last_hit.timestamp())?;
         Ok(mlua::Value::Table(history_entry))
     }
 }
