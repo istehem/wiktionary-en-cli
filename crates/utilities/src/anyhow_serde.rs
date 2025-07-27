baad41c830f4a77d09ed6ff58098096885eeac52
Ingemarsson Oskar <oskar@oskar.me>
2025-07-27 08:49:53 +0200

    use a where clause over inline type constraint

--- crates/utilities/src/anyhow_serde.rs
+++ crates/utilities/src/anyhow_serde.rs
@@ -1,10 +1,16 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
-pub fn from_str<T: for<'a> Deserialize<'a>>(line: &str) -> Result<T> {
+pub fn from_str<T>(line: &str) -> Result<T>
+where
+    for<'a> T: Deserialize<'a>,
+{
     serde_json::from_str(line).map_err(anyhow::Error::new)
 }
 
-pub fn to_string<T: Serialize>(value: &T) -> Result<String> {
+pub fn to_string<T>(value: &T) -> Result<String>
+where
+    T: Serialize,
+{
     serde_json::to_string(value).map_err(anyhow::Error::new)
 }
